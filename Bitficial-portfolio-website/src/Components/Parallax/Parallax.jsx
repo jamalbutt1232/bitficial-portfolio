abc151748554c7ca53d13676166f776075247631
jawad1529 <syedjawadshah00@gmail.com>
2024-03-30 16:55:39 +0500

    Parallax modified but framer motion is giving some issues

--- Bitficial-portfolio-website/src/Components/Parallax/Parallax.jsx
+++ Bitficial-portfolio-website/src/Components/Parallax/Parallax.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./parallax.scss";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 const Parallax = ({ type }) => {
+const ref = useRef()
+
+
+
+const {scrollyprogress}=useScroll({
+  target: ref,
+  
+})
+
+
+const yBg=useTransform(scrollyprogress, [0,1], ["0%", "100%"])
+
   return (
     <div
       className="Parallax"
@@ -12,7 +25,7 @@ const Parallax = ({ type }) => {
             : "linear-gradient(180deg, #111132 , #505064)",
       }}
     >
-      <motion.h1>
+      <motion.h1 style={{y:yBg}}>
         {type === "services" ? "What we did?" : "What we do?"}
       </motion.h1>
       <motion.div className="mountains">1</motion.div>
